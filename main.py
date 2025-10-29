99282e8e32be1c5a05ea694e1890caf58b037caf
ZerozCain <engineerabbe@gmail.com>
2025-10-29 16:44:38 +0100

    fixed to include any book you want to analyze.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from stats import get_book_text
 from stats import char_count
 from stats import char_count_sort
-
+import sys
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    print(f"Found {len(get_book_text('books/frankenstein.txt').split())} total words")
+    print(f"Found {len(get_book_text(sys.argv[1]).split())} total words")
     print("-------- Character Count -------")
-    for item in char_count_sort(char_count(get_book_text('books/frankenstein.txt'))):
+    for item in char_count_sort(char_count(get_book_text(sys.argv[1]))):
         print(f"{item["char"]}: {item["num"]}")
     
 
